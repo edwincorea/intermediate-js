f69cd56c4e58a40dbd57fb1c811596cc29703628
edwin.corea@gmail.com <edwin.corea@gmail.com>
2016-05-19 12:21:19 -0600

    Scoping

--- playground/index-01-scopes.js
+++ playground/index-01-scopes.js
@@ -1,1 +1,17 @@
-console.log("Hello, World");
+// ---------------------
+// Example 1
+var people = ["Edwin", "Jon", "Joan", "James"];
+var list = document.getElementById("people-list");
+
+for(var i = 0; i < people.length; i++){
+    var person = people[i];
+    var element = document.createElement("li");
+    element.innerText = person;    
+    
+    //scoping issue: all items show the last: "James"
+    element.addEventListener("click", function() {
+        alert("You clicked on " + person + ", at index " + i);        
+    });
+    
+    list.appendChild(element);
+}
